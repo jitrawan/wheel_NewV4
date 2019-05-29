f85cdd5f0a5852f0cea2c5b06458e9446254c22a
jitrawan <41459948+jitrawan@users.noreply.github.com>
2019-05-29 14:13:17 +0700

    commit

---
+++ core/config.core.php
@@ -0,0 +1,13 @@
+<?php 
+// MySQL hostname
+define('DB_HOST', 'localhost');
+
+// MySQL database username
+define('DB_USERNAME', 'root');
+
+// MySQL database password
+define('DB_PASSWORD', '12345678');
+
+// MySQL database name
+define('DB_NAME', 'wheel'); 
+?>
